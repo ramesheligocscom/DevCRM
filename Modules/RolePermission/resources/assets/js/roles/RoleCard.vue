e5d9a600daea7c8872ce1ea6a39127f219c075bc
Resham singh <resham@eligocs.com>
2025-04-08 10:49:19 +0530

    update permission file

--- Modules/RolePermission/resources/assets/js/roles/RoleCard.vue
+++ Modules/RolePermission/resources/assets/js/roles/RoleCard.vue
@@ -1,88 +1,101 @@
 <template>
-    <VRow>
-        <!-- 👉 Roles -->
-        <VCol v-for="role in roleList" :key="role.id" cols="12" sm="6" lg="4">
-            <VCard>
-                <VCardText class="d-flex align-center pb-4">
-                    <div class="text-body-1">
-                        <VChip variant="tonal" color="primary">
-                            {{ role.users.length }} {{ role.users.length === 1 ? 'user' : 'users' }}
-                        </VChip>
-                    </div>
-                    <VSpacer />
-                    <div class="v-avatar-group">
-                        <template v-for="(user, index) in role.users" :key="user.id+'-'+index">
-                            <v-tooltip location="top">
-                                <template v-slot:activator="{ props }">
-                                    <VAvatar v-if="role.users.length > 4 && role.users.length !== 4 && index < 3"
-                                        v-bind="props" size="40" :image="user.image" />
-                                    <VAvatar v-else v-bind="props" size="40" :image="user.image" />
-                                </template>
-                                {{ user.name }}
-                            </v-tooltip>
-                        </template>
-                        <VAvatar v-if="role.users.length > 4"
-                            :color="$vuetify.theme.current.dark ? '#373B50' : '#EEEDF0'">
-                            <span> +{{ role.users.length - 3 }} </span>
-                        </VAvatar>
-                    </div>
-                </VCardText>
+    <div>
+        <VRow>
+            <!-- 👉 Roles -->
+            <VCol v-for="role in roleList" :key="role.id" cols="12" sm="6" lg="4">
+                <VCard>
+                    <VCardText class="d-flex align-center pb-4">
+                        <div class="text-body-1">
+                            <VChip variant="tonal" color="primary">
+                                {{ role.users.length }} {{ role.users.length === 1 ? 'user' : 'users' }}
+                            </VChip>
+                        </div>
+                        <VSpacer />
+                        <div class="v-avatar-group">
+                            <template v-for="(user, index) in role.users" :key="user.id+'-'+index">
+                                <v-tooltip location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <VAvatar v-if="role.users.length > 4 && role.users.length !== 4 && index < 3"
+                                            v-bind="props" size="40" :image="user.image" />
+                                        <VAvatar v-else v-bind="props" size="40" :image="user.image" />
+                                    </template>
+                                    {{ user.name }}
+                                </v-tooltip>
+                            </template>
+                            <VAvatar v-if="role.users.length > 4"
+                                :color="$vuetify.theme.current.dark ? '#373B50' : '#EEEDF0'">
+                                <span> +{{ role.users.length - 3 }} </span>
+                            </VAvatar>
+                        </div>
+                    </VCardText>
 
-                <VCardText>
-                    <div class="d-flex justify-space-between align-center">
-                        <div class="text-left">
-                            <h5 class="text-h5"> {{ role.name }} </h5>
+                    <VCardText>
+                        <div class="d-flex justify-space-between align-center">
+                            <div class="text-left">
+                                <h5 class="text-h5"> {{ role.name }} </h5>
+                            </div>
+                            <div class="text-right">
+                                <v-tooltip location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <Router-link v-bind="props" :to="`/role-permission/edit/${role.id}`"
+                                            v-if="$can('role', 'edit')">
+                                            <VIcon icon="tabler-edit" class="text-high-emphasis" color="primary"
+                                                variant="text" />
+                                        </Router-link>
+                                    </template>
+                                    Edit Role
+                                </v-tooltip>
+                                <v-tooltip location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <IconBtn v-bind="props" @click="duplicateRoleCreate(role)"
+                                            v-if="$can('role', 'create')">
+                                            <VIcon icon="tabler-copy" class="text-high-emphasis" color='info'
+                                                variant="text" />
+                                        </IconBtn>
+                                    </template>
+                                    Duplicate Role
+                                </v-tooltip>
+                                <v-tooltip location="top" v-if="role.slug != 'super-admin' && $can('role', 'delete')">
+                                    <template v-slot:activator="{ props }">
+                                        <IconBtn v-if="$can('role', 'delete')" v-bind="props"
+                                            @click="openDeleteDialog(role)">
+                                            <VIcon icon="tabler-trash" class="text-high-emphasis" color='error'
+                                                variant="text" />
+                                        </IconBtn>
+                                    </template>
+                                    Delete Role
+                                </v-tooltip>
+                            </div>
                         </div>
-                        <div class="text-right">
-                            <v-tooltip location="top">
-                                <template v-slot:activator="{ props }">
-                                    <Router-link v-bind="props" :to="`/role-permission/edit/${role.id}`"
-                                        v-if="$can('role', 'edit')">
-                                        <VIcon icon="tabler-edit" class="text-high-emphasis" color="primary"
-                                            variant="text" />
-                                    </Router-link>
-                                </template>
-                                Edit Role
-                            </v-tooltip>
-                            <v-tooltip location="top">
-                                <template v-slot:activator="{ props }">
-                                    <IconBtn v-bind="props" @click="duplicateRoleCreate(role)"
-                                        v-if="$can('role', 'create')">
-                                        <VIcon icon="tabler-copy" class="text-high-emphasis" color='info'
-                                            variant="text" />
-                                    </IconBtn>
-                                </template>
-                                Duplicate Role
-                            </v-tooltip>
-                            <v-tooltip location="top" v-if="role.slug != 'super-admin'">
-                                <template v-slot:activator="{ props }">
-                                    <IconBtn v-bind="props" @click="deleteUser(role.id, 'force_delete')"
-                                        v-if="$can('role', 'delete')">
-                                        <VIcon icon="tabler-trash" class="text-high-emphasis" color='error'
-                                            variant="text" />
-                                    </IconBtn>
-                                </template>
-                                Delete Role
-                            </v-tooltip>
-                        </div>
-                    </div>
-                </VCardText>
-            </VCard>
-        </VCol>
-    </VRow>
+                    </VCardText>
+                </VCard>
+            </VCol>
+        </VRow>
+        <!-- 👉 Delete Dialog -->
+        <DeleteDialog v-model:isDialogVisible="isDeleteDialogOpen" confirm-title="Delete!"
+            confirmation-question="Are you sure want to delete role?" :currentItem="currentInfo"
+            @submit="fetchRoleList" :endpoint="`/role/${currentInfo?.id}`" @close="isDeleteDialogOpen = false" />
+    </div>
 </template>
 <script setup>
 import Swal from 'sweetalert2';
 import { onMounted } from 'vue';
 import { toast } from 'vue3-toastify';
 import { useTheme } from 'vuetify';
+import DeleteDialog from './dialog/DeleteDialog.vue';
 
 const vuetifyTheme = useTheme()
 const currentTheme = vuetifyTheme.current.value.colors
-const props = defineProps({
-    roleList: { type: Array, default: [] },
-});
+const isDeleteDialogOpen = ref(false)
+const currentInfo = ref(null);
+
+const props = defineProps({ roleList: { type: Array, default: [] }, });
+
 const emit = defineEmits(['refreshRoleList']);
+
+const fetchRoleList = async (response) => {
+    emit('refreshRoleList', response.data);
+};
 
 const duplicateRoleCreate = async (role) => {
     let title = 'Are you sure you Create Duplicate Role';
@@ -114,6 +127,11 @@ const duplicateRoleCreate = async (role) => {
             toast.error(error?._data?.message ?? "Error occurred while assigning roles.");
         }
     }
+}
+
+const openDeleteDialog = (item) => {
+    currentInfo.value = item;
+    isDeleteDialogOpen.value = true;
 }
 
 const deleteUser = async (role_id, action) => {
@@ -196,7 +214,6 @@ const deleteUser = async (role_id, action) => {
 };
 
 onMounted(() => {
-    //console.log("currentTheme", currentTheme);
 });
 
 </script>
